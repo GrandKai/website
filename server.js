6355bfae5d17c8829d0817a2ac2f1472cce62b62
GrandKai <GrandKai@aliyun.com>
2017-06-15 18:51:48 +0800

    upgrade express4

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-// var config = require('./server/configure');
+var config = require('./server/configure');
 var app = express();
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
-// app = config(app);
+app = config(app);
 
 var server = app.listen(app.get('port'), function() {
   console.log('server up: http://localhost:' + app.get('port'));
--- server/configure.js
+++ server/configure.js
@@ -1,7 +1,12 @@
-var connect = require('connect'),
-  path = require('path'), 
-  // routes = require('./routes')
-  exphbs = require('express3-handlebars');
+var path = require('path'),
+  routes = require('./routes'),
+  exphbs = require('express3-handlebars'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  morgan = require('morgan'),
+  methodOverride = require('method-override'),
+  errorHandler = require('errorhandler');
 
 module.exports = function (app) {
   // configuration code...
@@ -13,5 +18,30 @@ module.exports = function (app) {
   }).engine);
   app.set('view engine', 'handlebars');
 
+  // 服务器收到的每个 request 请求以 console.log() 的形式输出
+  app.use(morgan('dev'));
+
+  // 方便打包通过浏览器 POST 提交的表单字段（通过 req.body 属性）
+  // 同 bodyParser 处理posted JSON data via the req.body property
+  // 同 bodyParser get 提交的表单字段 req.body
+  app.use(bodyParser({
+    uploadDir: path.join(__dirname, '../public/upload/temp')
+  }));
+
+  // 兼容老版本浏览器不支持 REST HTTP 动作（如： update，put），methodOverride 允许通过隐藏表单域伪造
+  app.use(methodOverride());
+  // This allows cookies to be sent/received.
+  app.use(cookieParser('some-secret-value-here'));
+  routes.initialize(app, new express.Router());
+  // app.use(app.router);
+
+  // server up static resource
+  app.use('/public/', express.static(path.join(__dirname, '../public')));
+
+  if ('development' === app.get('env')) {
+    // 异常处理，自定义错误页面，存储异常信息等
+    app.use(errorHandler());
+  }
+
   return app;
-}
+};
